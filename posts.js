bc5ace32bbae5ba4c49d4d3aae8481e986a833cf
Breakfulus <130721537+Breakfulus@users.noreply.github.com>
2026-03-21 21:36:23 -0400

    Added devlog page
    Devlog posts are made in the new posts.js and stored as an array, then loaded to the post and devlog pages. Both pages sort the list by date and whether the post is pinned, then devlog renders it as a list where you can click to view entries.

---
+++ posts.js
@@ -0,0 +1,15 @@
+window.posts = [
+    {
+        title: "Welcome to the devlog board!",
+        date: "2026-03-21",
+        pinned: true,
+        type: "Announcement",
+        content: `
+        <p>Hiya! This is my new devlog board, exciting isn't it? Since Ive been working on a game for the past few months, I decided I wanted to start sharing it. It will probably take me a bit
+        to actually make the first devlog showcasing what I've gotten done so far and explaining the project, but I'll make sure to get on that ASAP! I'm just preimptively opening up the dev board so 
+        I dont gotta later. I also just wanted to do it since I haven't messed with JS yet, and this was a good oppurtunity to dive into it. It's definitely a lot different from python and Lua, thats 
+        for sure!<br><br>This board is going to be for any and all developement projects im working on, so if I start working on a side project, like this site, I will also post devlogs aboutsaid side
+         project here too. I'm sure the way I'm going about this devlog page thingy is a lot more complicated and could be done a lot better, but I'll figure it out in time.</p>
+        `
+    },
+]
