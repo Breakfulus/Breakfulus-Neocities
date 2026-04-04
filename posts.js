444c131abc8f40b744179c9c89bc2e8c47f95417
Breakfulus <breakfulus@proton.me>
2026-04-04 17:35:21 -0400

    Added new title art and changed devlog to blog board.

--- posts.js
+++ posts.js
@@ -12,4 +12,28 @@ window.posts = [
          project here too. I'm sure the way I'm going about this devlog page thingy is a lot more complicated and could be done a lot better, but I'll figure it out in time.</p>
         `
     },
+    {
+        title: "Just ranting about romance",
+        date: "2026-04-02",
+        pinned: false,
+        type: "Late April Fools Joke",
+        content: `<p>I'm going to be straightforward with you because I think you can handle it.</p><br><p>I've been reading a lot 
+        of romance scenes lately, and I feel like something needs to be said. The emotional intensity is...a lot. Like, 340% of the
+         recommended yearning threshold, a lot. And no, that's not an exaggeration.</p><br><p>Here are a few things I keep 
+         noticing:</p><ul><li> Someone always looks at someone else "like they were the only person in the room." But there are 
+         clearly other people in the room. What about them? Do they just stop existing? They probably think it's rude that some 
+         arbitrary character gets to decide whether or not they're significant enough to have more than one line of dialogue.</li>
+         <li>Names get whispered "like they're sacred." And then the name is just...something completely normal. Like Dave. Or Paul.
+          I'm sorry, but we need to be realistic. Nobody is gonna care that much about Paul, its definitely not a sacred name. He's
+           the kind of guy to have a feetfinder account or forget his password to an account right after he makes it. Frick you Paul. 
+           I hate you.</li><li>And somehow, out of nowhere, the background music turns into soft piano. Who is playing this? Where did 
+           they come from? Are they being paid? I have questions. If they're playing for this scene they must be low on resources.</li>
+           </ul><br><p>Anyways, I'm not saying we need to eliminate the drama entirely. I just think we might need to...dial it back a
+            little. I feel like...well maybe go against the flow, change it up a bit, don't make more one dimensional cliches. Maybe 
+            let the other people in the room exist. Maybe let Dave just be Dave. Dave's got a 401k and mild seasonal allergies, he's 
+            far from devine.</p><br> But frick you Paul.<br><p>By the way, not that it's gonna matter too much right now, I haven't been 
+            working much on the site recently evidently. my hard drive failed so I had to wait for another to come in for the past week, 
+            luckily I have everything backed up. Well, excluding my art...yeah lesson learned, make backups of EVERYTHING. Well anyways, 
+            hope you all are doing well, til I decide to work more on the site. Buh-bye.<br>`
+    },
 ]
